e3e658cadf38e2b5b77f279e3a5a2426f114bbb4
saltyaom <saltyaom@gmail.com>
2022-01-16 12:04:32 +0700

    :tada: feat: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hifumin comment mirror
+Similar to [Hifumin Mirror](https://github.com/saltyaom-engine/hifumin-mirror) but for mirroring comments.
+
+nHentai comment mirror, this repo generate comment's mirror of nHentai API every 2 days, available at [generated](https://github.com/saltyaom-engine/hifumin-mirror/tree/generated) branch.
+
+## Why
+Hifumin usually exceed nHentai rate limit even with local cache, running both [hifumin.app](https://hifumin.app) and [Hifumin API](https://api.hifumin.app), so I create this mirror archive instead to fully evade from nHentai.
+
+## Concept
+Using Github Actions free plan, I can spin up 20 concurrent workers pool and seperate scarp task into a batch.
+
+As NHentai has rate limiting, estimate time mirroring for 370,000 hentais is around 1 hour and a 20 minutes.
---
+++ src/index.ts
@@ -0,0 +1,162 @@
+import { mkdirSync, existsSync, writeFileSync } from 'fs'
+
+import PQueue from 'p-queue'
+import fetch from 'isomorphic-unfetch'
+import parse from 'node-html-parser'
+
+// NHentai Rate limit
+const queue = new PQueue({ concurrency: 6 })
+
+// ? Get estimate latest nhentai id
+const getLatest = async (): Promise<number | Error> => {
+    const html = await fetch('https://nhentai.net')
+        .then((res) => res.text())
+        .then((res) => parse(res))
+
+    const firstCover = html.querySelector(
+        '#content > .index-container:nth-child(2) > .gallery > .cover'
+    )
+
+    if (!firstCover) throw new Error("Couldn't find first cover")
+
+    const url = firstCover.getAttribute('href')!
+
+    const id = url
+        .split('/')
+        .reverse()
+        .find((x) => x)
+
+    return id ? parseInt(id) : new Error("Couldn't find id")
+}
+
+const getNhentai = async (
+    id: number,
+    iteration = 0
+): Promise<string | Error> => {
+    try {
+        const hentai: string = await fetch(
+            `https://nhentai.net/api/gallery/${id}/comments`
+        ).then((res) => res.text())
+
+        if (hentai === "[]") return "[]"
+        if (!hentai.startsWith('[{"id"')) return new Error('Not found')
+
+        return hentai
+    } catch (err) {
+        if (iteration <= 5) {
+            await new Promise((resolve) => setTimeout(resolve, 8000))
+
+            return getNhentai(id, iteration + 1)
+        }
+
+        return new Error("Couldn't fetch hentai")
+    }
+}
+
+const estimateTime = ({
+    since,
+    current,
+    total
+}: {
+    since: number
+    current: number
+    total: number
+}) => (((performance.now() - since) / current) * (total - current)) / 1000
+
+const formatDisplayTime = (time: number) => {
+    let seconds = ~~time
+    let minutes = 0
+    let hours = 0
+
+    while (seconds >= 3600) {
+        seconds -= 3600
+        hours += 1
+    }
+
+    while (seconds >= 60) {
+        seconds -= 60
+        minutes += 1
+    }
+
+    if (hours) return `${hours}h ${minutes}m ${seconds}s`
+
+    if (minutes) return `${minutes}m ${seconds}s`
+
+    return `${seconds}s`
+}
+
+const batch = (
+    total: number,
+    batch: number = +(process.env?.WORKER_INDEX ?? 1)
+) => {
+    const totalWorker = +(process.env?.WORKER_COUNT ?? 1)
+
+    const start = Math.floor(((batch - 1) * total) / totalWorker + 1)
+    const end = Math.floor((batch * total) / totalWorker)
+
+    return { start, end }
+}
+
+const main = async () => {
+    const total = await getLatest()
+
+    if (total instanceof Error) {
+        console.error(total.message)
+        process.exit(1)
+    }
+
+    const { start, end } = batch(total)
+    console.log(`${total} total, worker: ${start} - ${end}`)
+
+    if (!existsSync('data')) mkdirSync('data')
+
+    const since = performance.now()
+
+    let current = start
+    let iteration = 1
+
+    for (let i = start; i <= end; i++) {
+        queue.add(async () => {
+            const hentai = await getNhentai(i)
+
+            current++
+            iteration++
+
+            if (hentai instanceof Error) {
+                console.log(`${i} not found`)
+                return
+            }
+
+            writeFileSync(`data/${i}.json`, hentai)
+        })
+
+        // For GH Action use 1.25s, local use 0.625s
+        queue.add(() => new Promise((resolve) => setTimeout(resolve, 1250)))
+    }
+
+    const progress = setInterval(() => {
+        console.log(
+            `(${((iteration / (end - start)) * 100).toFixed(
+                4
+            )}%) | ${current}/${end} | Estimate time left: ${formatDisplayTime(
+                estimateTime({
+                    current: iteration,
+                    total: end - start,
+                    since
+                })
+            )}`
+        )
+    }, 10000)
+
+    await queue.onIdle()
+
+    clearInterval(progress)
+
+    console.log(
+        'Done in',
+        ((performance.now() - since) / 1000).toFixed(3),
+        'seconds'
+    )
+}
+
+main()
